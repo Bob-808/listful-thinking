f377608f366965d5bad2b982f057c00c3cf54e78
Bob-808 <bob99anderson99@gmail.com>
2026-06-11 18:14:10 -0400

    1.3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let currentQuestion = null;
 let foundAnswers = [];
 let shuffledQuestions = [];
 let currentQuestionIndex = 0;
-let Release = 1.2;
+let Release = 1.3;
 let DEV_MODE = false;
 
 let scores = {};
